e71db1a22ca3f66ca2e912cc5f8ed8d52cd35caa
TvanSpa <thomas.vanspaendonk@epfl.ch>
2024-11-15 21:20:40 +0100

    directory and file verification in pipeline added

--- src/streamliningData/pipeline.py
+++ src/streamliningData/pipeline.py
@@ -1,5 +1,22 @@
 import papermill as pm
 import logging
+import os
+
+def verify_file_exists(file_path):
+    if not os.path.exists(file_path):
+        print(f"Error: The file does not exist: {file_path}")
+        print("Stopping the execution.")
+        exit()  # Stop the script execution if file does not exist
+    else:
+        print(f"The file exists: {file_path}")
+
+def verify_and_create_directory(directory_path):
+    if not os.path.exists(directory_path):
+        print(f"The directory does not exist: {directory_path}")
+        print(f"Creating the directory: {directory_path}")
+        os.makedirs(directory_path)  # Create the directory (and parents if needed)
+    else:
+        print(f"The directory already exists: {directory_path}")
 
 #I found this logger code online in github issue post, it works to suppress what I want
 class PapermillFilter(logging.Filter):
@@ -26,6 +43,38 @@ notebook_paths = [
     
     #add extract_php.ipynb
 ]
+
+
+files_to_check = [
+    'baseData/BeerAdvocate/beers.csv',
+    'baseData/BeerAdvocate/breweries.csv',
+    'baseData/BeerAdvocate/ratings.txt',
+    'baseData/BeerAdvocate/reviews.txt',
+    'baseData/BeerAdvocate/users.csv',
+    'baseData/RateBeer/beers.csv',
+    'baseData/RateBeer/breweries.csv',
+    'baseData/RateBeer/ratings.txt',
+    'baseData/RateBeer/reviews.txt',
+    'baseData/RateBeer/users.csv',
+]
+
+directories_to_check = [
+    'additionalData',
+    'minimizedData',
+    'USData',
+]
+
+for file in files_to_check:
+    verify_file_exists(file)
+
+for directory in directories_to_check:
+    verify_and_create_directory(directory)
+
+for directory in directories_to_check:
+    if not os.path.exists(directory):
+        print(f"Warning: Directory '{directory}' does not exist.")
+    else:
+        print(f"Directory '{directory}' exists.")
 
 print('\nIt takes around 12 minutes to run everything \n')
 
